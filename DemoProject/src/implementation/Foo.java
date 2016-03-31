c1e1452a976bd6a639f6a6211034bb5d1cb82ef8
Salome Maro <salomem@chalmers.se>
2016-03-31 10:47:50 +0200

    Added demo project

---
+++ DemoProject/src/implementation/Foo.java
@@ -0,0 +1,14 @@
+package implementation;
+
+public class Foo {
+
+	public Foo() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ DemoProject/src/tests/Footest.java
@@ -0,0 +1,14 @@
+package tests;
+
+public class Footest {
+
+	public Footest() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
